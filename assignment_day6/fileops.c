e5109c2c54792138c063a71282c27ac09a8fb2c3
SREERAMULA-SAIKUMAR <sreeramulasaikumar@gmail.com>
2021-11-30 09:47:50 +0530

    system call functions updated

--- assignment_day6/fileops.c
+++ assignment_day6/fileops.c
@@ -3,6 +3,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include<stdlib.h>
+
 int main()
 {
     int fd;
@@ -11,7 +13,9 @@ int main()
     if(-1==fd)
     {
         printf("Error\n");
+        exit(EXIT_FAILURE);
     }
+
     write(fd,"DESD",4);
     close(fd);
     return 0;
