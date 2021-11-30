cf274eb759e04abcdcd5ac9f7834ad9f285323f2
SREERAMULA-SAIKUMAR <sreeramulasaikumar@gmail.com>
2021-11-30 19:09:20 +0530

    implemented process id program

--- assignment_day5/pid.c
+++ assignment_day5/pid.c
@@ -7,5 +7,8 @@ int main(int argc, char *argv[])
     pid_t id,pa_id;
     id=getpid();
     pa_id=getppid();
-    printf("process id = %d\nparent process id = %d\n",id,pa_id);
+
+    printf("process id = %d\n",id);
+    printf("parent process id = %d\n",pa_id);
 }
+
