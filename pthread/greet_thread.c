a59a45421bf65179d0c4faca20c0ebc7b089f4fd
SREERAMULA-SAIKUMAR <sreeramulasaikumar@gmail.com>
2021-12-03 14:41:30 +0530

    hello bye threads added

---
+++ pthread/greet_thread.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <pthread.h>
+
+void *greet_thread(void *arg)
+{
+    printf("%s thread\n", arg);
+}
+
+int main(int argc, char const *argv[])
+{
+    pthread_t helloID, byeID;
+    printf("Main thread: Before creating hello thread\n");
+
+    pthread_create(&helloID, NULL, greet_thread, "Hello");
+    pthread_create(&byeID, NULL, greet_thread, "Bye");
+    pthread_join(helloID, NULL);
+    pthread_join(byeID, NULL);
+
+    printf("Main thread: After creating hello thread\n");
+
+    return 0;
+}
+
