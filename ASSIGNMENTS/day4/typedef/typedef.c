f8bcc72be40321a205ccc9c7f1484f055b5cfbe9
SREERAMULA-SAIKUMAR <sreeramulasaikumar@gmail.com>
2021-12-07 12:50:36 +0530

    updated typedef assignment programs

---
+++ ASSIGNMENTS/day4/typedef/typedef.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  
+int main()  
+{  
+    typedef unsigned int unit;  
+    unit i,j;  
+    i=10;  
+    j=20;  
+    printf("Value of i is: %d\n",i);  
+    printf("Value of j is: %d\n",j);  
+    
+    return 0;  
+}
+
